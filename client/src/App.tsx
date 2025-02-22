27457dec31af24a4c1d11ec9a26a0b63d25381bc
RamAvni <ramavni@hilma.tech>
2025-02-22 15:41:57 +0200

    feat(client): added the home page file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>hi</h1>,
+    element: <Home />,
   },
 ]);
 
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Home: FC = () => {
+  return <h1>hi</h1>;
+};
+
+export default Home;
